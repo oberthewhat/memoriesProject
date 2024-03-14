0b8a9124170b9d94d957fd1ca55ff610e579ae95
John Obertubbesing <oberthewhat@gmail.com>
2024-03-14 18:47:08 -0500

    added controllers

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,36 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import brandLogo from './images/thrillnerds-logo-02-small.png'
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import useStyles from "./styles";
 
 const App = () => {
+    const classes = useStyles();
     return (
-        <div>
-            <h1>App</h1>
-        </div> 
-    )
+        <Container maxWidth="lg">
+            <AppBar className = {classes.appBar} position="static" color="inherit">
+                <Typography className="{classes.heading}" variant="h2" align="center">
+                    ThrillNerds
+                </Typography>
+                <img classNames={classes.image} src={brandLogo} alt="ThrillNerds" height="60" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
+
+        )
+
 }
 
 export default App;
